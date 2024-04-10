4c036edffb5fcb73ecf36549cabc641b78d183c7
mariohzz <amrohoney40@yahoo.com>
2024-04-10 14:21:46 +0200

    Add project files.

---
+++ ConsoleApp1/LinearArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	class LinearArray
+	{
+		private int[] data;
+		private static Random generator = new Random();
+		public LinearArray(int size)
+		{
+			//create a space for array
+			data = new int[size];
+			//fill Array with randim ints in range 10-99
+			for (int i = 0; i < size; i++)
+				data[i] = generator.Next(10, 100);
+		}
+
+		//perform a linear search on the data
+		public int LinearSearch(int SearchKey)
+		{
+			//loop through array squentially
+			for (int i = 0; i < data.Length; ++i)
+				if (data[i] == SearchKey)
+					return i;
+
+			return -1; //else intger was not found
+		}//end mathod linear search
+
+		//method to output values array
+		public override string ToString()
+		{
+			string temorary = string.Empty;
+			//itreate thrugh array
+			foreach (var item in data)
+				temorary += item + " ";
+
+			temorary += "\n";  //add newline characte
+			return temorary;
+		}
+	}
+
+
+}
+
+
---
+++ ConsoleApp1/TestLinearSearch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class TestLinearSearch
+    {
+        static void Main(string[] args)
+        {
+            int searchInt; //search Key
+            int position; //loction of search key in array
+
+            //create array and output it
+            LinearArray SearchArray = new LinearArray(10);
+            //input frist int from user
+            Console.WriteLine(SearchArray);
+
+            //create array and output it
+            Console.Write("please enter an intger value ( -1 to quit): ");
+            searchInt = Convert.ToInt32(Console.ReadLine()); //display array
+
+            //repeatedly input an intger; -1 trminates the app
+            while (searchInt != -1)
+            {
+                //perform linear search
+                position = SearchArray.LinearSearch(searchInt);
+                if (position != -1)
+                    Console.WriteLine("the intger {0} was found in postoin {1}. \n" ,
+                        searchInt,position
+                        );
+                else //intger was not found
+                    Console.WriteLine("the intger {0} was not found in postoin {1}. \n",
+                        searchInt
+                        );
+                //input next int from user
+                Console.Write("please enter an integer value (-1 to quite): ");
+                searchInt = Convert.ToInt32(Console.ReadLine());
+            }
+
+        }
+    }
+ }
+
+
+
